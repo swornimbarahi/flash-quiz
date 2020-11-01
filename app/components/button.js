86b76be6345b6942f9f979842e3f68be8633947c
swornimbarahi <swornimbarahi@gmail.com>
2020-11-01 11:23:51 -0500

    feat(components): added button classnames

---
+++ app/components/button.js
@@ -0,0 +1,12 @@
+import { computed } from "@ember/object";
+import Component from "@ember/component";
+import classnames from "classnames";
+
+export default Component.extend({
+  tagName: '',
+  className: computed("button.{size,inverted,danger}", function () {
+    let { size = "md", inverted = false, danger = false } = this;
+
+    return classnames([size, { inverted: !!inverted, danger: !!danger }]);
+  })
+});
